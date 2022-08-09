1a6eb046c48ffdb672438d56ebd0183674c6e70c
Supanut1911 <ynotmela@gmail.com>
2022-08-09 16:28:35 +0700

    🔧 [chore]: connect kafka

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'HERO_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'hero',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'Publisher-app',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [
+          'localhost:9092',
+          // process.env.KAFKA_BROKER_2,
+          // process.env.KAFKA_BROKER_3,
+        ],
+      },
+      consumer: {
+        groupId: 'Publisher-app',
+      },
+    },
+  });
+  await app.startAllMicroservices();
+
+  await app.listen(4001);
 }
 bootstrap();
